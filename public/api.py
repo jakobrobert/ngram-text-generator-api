96178cf1f3f0bbec72ab26cfea80440b6e26f291
Jack-42 <jakob-robert@hotmail.de>
2020-10-25 23:46:45 +0100

    Create dummy GET endpoints

--- public/api.py
+++ public/api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,30 @@ app = Flask(__name__)
 @app.route("/ngram-text-generator-api")
 def index():
     return "Hello World!"
+
+
+# TODO: change to POST with json body
+# TODO: return ngram model as json
+@app.route("/ngram-text-generator-api/build-model", methods=['GET'])
+def build_model():
+    order = int(request.args.get('order'))
+    training_text = request.args.get('training-text')
+    return jsonify(
+        order=order,
+        training_text=training_text
+    )
+
+
+# TODO: change to POST with json body
+# TODO: provide ngram model as json
+@app.route("/ngram-text-generator-api/generate-text", methods=['GET'])
+def generate_text():
+    start_text = request.args.get("start-text")
+    text_length = int(request.args.get("text-length"))
+    return jsonify(
+        start_text=start_text,
+        text_length=text_length
+    )
 
 
 if __name__ == "__main__":
