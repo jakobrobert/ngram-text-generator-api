c8cf6d75b322e8bb8f5852ee731bbc1a30be76fc
Jack-42 <jakob-robert@hotmail.de>
2020-10-31 23:26:42 +0100

    Tokenize text

--- public/api.py
+++ public/api.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, jsonify
-import json
+
+# special chars which are concatenated to the previous with a separation by space
+SPECIAL_CHARS_WITH_SEPARATION = ["(", "[", "{", "\""]
+
+# special chars which are directly concatenated to the previous token without any separation
+SPECIAL_CHARS_WITHOUT_SEPARATION = [".", "?", "!", ",", ";", ":", ")", "]", "}", "\n"]
+
+# all special chars, no distinction necessary for building the model
+SPECIAL_CHARS = SPECIAL_CHARS_WITH_SEPARATION + SPECIAL_CHARS_WITHOUT_SEPARATION
 
 app = Flask(__name__)
-
 
 @app.route("/ngram-text-generator-api")
 def index():
@@ -14,25 +21,74 @@ def build_model():
     request_data = request.json
     order = request_data["order"]
     training_text = request_data["training_text"]
+    tokens = preprocess_text(training_text)
     # TODO: replace dummy response by ngram model
     return jsonify(
         order=order,
-        training_text=training_text
+        tokens=tokens
     )
 
 
 @app.route("/ngram-text-generator-api/generate-text", methods=['POST'])
 def generate_text():
     request_data = request.json
+    model = request_data["model"]
     start_text = request_data["start_text"]
     text_length = request_data["text_length"]
-    model = request_data["model"]
     # TODO: replace dummy response by generated text
     return jsonify(
+        model=model,
         start_text=start_text,
-        text_length=text_length,
-        model=model
+        text_length=text_length
     )
+
+
+def preprocess_text(text):
+    filtered_text = filter_text(text)
+    tokens = tokenize(filtered_text)
+    # TODO: build dictionary
+    return tokens
+
+
+def filter_text(text):
+    # remove all "\r" (carriage return)
+    return text.replace("\r", "")
+
+
+def tokenize(text):
+    tokens = []
+    current = 0
+    token_start = 0
+
+    while current < len(text):
+        if text[current] == " ":
+            # take string before space as token (only if not empty)
+            if token_start < current:
+                token = text[token_start:current]
+                tokens.append(token)
+            # skip space
+            current += 1
+            token_start = current
+        elif text[current] in SPECIAL_CHARS:
+            # take string before special char as token (only if not empty)
+            if token_start < current:
+                token = text[token_start:current]
+                tokens.append(token)
+            # add special char as separate token
+            special_char = text[current]
+            tokens.append(special_char)
+            current += 1
+            token_start = current
+        else:
+            # no new token found, just continue with next char
+            current += 1
+
+    # add remaining part as last token (only if not empty)
+    if token_start < len(text):
+        token = text[token_start:]
+        tokens.append(token)
+
+    return tokens
 
 
 if __name__ == "__main__":
