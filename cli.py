f92fae97dcd58d04f01a56d245f5febd4736e31d
Jack-42 <jakob-robert@hotmail.de>
2020-11-15 20:06:17 +0100

    Parse cmd line args

---
+++ cli.py
@@ -0,0 +1,24 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Train an n-gram model based on a given text and generate a new text")
+    required_args = parser.add_argument_group("required arguments")
+    required_args.add_argument("-i", "--input", type=str, required=True,
+                               help="Path to the input file containing the training text")
+    required_args.add_argument("-o", "--output", type=str, required=True,
+                               help="Path to the output file to store the generated text")
+    required_args.add_argument("-r", "--order", type=str, required=True,
+                               help="The order of the n-gram model")
+    required_args.add_argument("-s", "--start", type=str, required=True,
+                               help="The start text as a coherent string, must be a (order-1)-gram which occurs\
+                               in the training text")
+    required_args.add_argument("-l", "--length", type=str, required=True,
+                               help="The desired text length in tokens")
+    args = parser.parse_args()
+
+    print(args)
+
+
+if __name__ == "__main__":
+    main()
