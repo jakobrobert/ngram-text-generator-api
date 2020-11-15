90967b4fad8e6fdc2eb5c01caaa4c4571fe9ccce
Jack-42 <jakob-robert@hotmail.de>
2020-11-15 20:08:43 +0100

    Fix types of cmd line args

--- cli.py
+++ cli.py
@@ -8,12 +8,12 @@ def main():
                                help="Path to the input file containing the training text")
     required_args.add_argument("-o", "--output", type=str, required=True,
                                help="Path to the output file to store the generated text")
-    required_args.add_argument("-r", "--order", type=str, required=True,
+    required_args.add_argument("-r", "--order", type=int, required=True,
                                help="The order of the n-gram model")
     required_args.add_argument("-s", "--start", type=str, required=True,
                                help="The start text as a coherent string, must be a (order-1)-gram which occurs\
                                in the training text")
-    required_args.add_argument("-l", "--length", type=str, required=True,
+    required_args.add_argument("-l", "--length", type=int, required=True,
                                help="The desired text length in tokens")
     args = parser.parse_args()
 
