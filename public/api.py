b793d8170da5907c0dccf47ae53ab47449a1867f
Jack-42 <jakob-robert@hotmail.de>
2020-11-30 21:12:11 +0100

    Use time.perf_counter() for more precise time measurement

--- public/api.py
+++ public/api.py
@@ -15,34 +15,34 @@ def index():
 
 @app.route("/ngram-text-generator-api/build-model", methods=['POST'])
 def build_model():
-    start_time = time.process_time()
+    start_time = time.perf_counter()
     request_data = request.json
     order = request_data["order"]
     training_text = request_data["training_text"]
-    elapsed_time = int((time.process_time() - start_time) * 1000)
+    elapsed_time = int((time.perf_counter() - start_time) * 1000.0)
     print("Deserialization (ms): " + str(elapsed_time))
 
-    start_time = time.process_time()
+    start_time = time.perf_counter()
     filtered_text = TextProcessor.filter_text(training_text)
     tokens = TextProcessor.tokenize(filtered_text)
     dictionary = TextProcessor.build_dictionary(tokens)
     token_ids = TextProcessor.convert_tokens_from_string_to_id(tokens, dictionary)
-    elapsed_time = int((time.process_time() - start_time) * 1000)
+    elapsed_time = int((time.perf_counter() - start_time) * 1000.0)
     print("Pre-processing (ms): " + str(elapsed_time))
 
-    start_time = time.process_time()
+    start_time = time.perf_counter()
     model = NGramModel(order)
     model.build_model_from_tokens(token_ids)
-    elapsed_time = int((time.process_time() - start_time) * 1000)
+    elapsed_time = int((time.perf_counter() - start_time) * 1000.0)
     print("Build model (ms): " + str(elapsed_time))
 
-    start_time = time.process_time()
+    start_time = time.perf_counter()
     response = jsonify(
         model=model.to_dict(),
         dictionary=dictionary.to_dict(),
         token_count=len(tokens)
     )
-    elapsed_time = int((time.process_time() - start_time) * 1000)
+    elapsed_time = int((time.perf_counter() - start_time) * 1000.0)
     print("Serialization (ms): " + str(elapsed_time))
 
     return response
@@ -50,36 +50,36 @@ def build_model():
 
 @app.route("/ngram-text-generator-api/generate-text", methods=['POST'])
 def generate_text():
-    start_time = time.process_time()
+    start_time = time.perf_counter()
     request_data = request.json
     start_text = request_data["start_text"]
     length = request_data["length"]
     model = NGramModel.from_dict(request_data["model"])
     dictionary = Dictionary.from_dict(request_data["dictionary"])
-    elapsed_time = int((time.process_time() - start_time) * 1000)
+    elapsed_time = int((time.perf_counter() - start_time) * 1000.0)
     print("Deserialization (ms): " + str(elapsed_time))
 
     # only a few tokens, not worth measuring time
     start_history_tokens = TextProcessor.tokenize(start_text)
     start_history_ids = TextProcessor.convert_tokens_from_string_to_id(start_history_tokens, dictionary)
 
-    start_time = time.process_time()
+    start_time = time.perf_counter()
     token_ids = model.generate_tokens(start_history_ids, length)
-    elapsed_time = int((time.process_time() - start_time) * 1000)
+    elapsed_time = int((time.perf_counter() - start_time) * 1000.0)
     print("Generate tokens (ms): " + str(elapsed_time))
 
-    start_time = time.process_time()
+    start_time = time.perf_counter()
     tokens = TextProcessor.convert_tokens_from_id_to_string(token_ids, dictionary)
     text = TextProcessor.concat_tokens_to_text(tokens)
-    elapsed_time = int((time.process_time() - start_time) * 1000)
+    elapsed_time = int((time.perf_counter() - start_time) * 1000.0)
     print("Post-processing (ms): " + str(elapsed_time))
 
-    start_time = time.process_time()
+    start_time = time.perf_counter()
     response = jsonify(
         text=text,
         token_count=len(tokens)
     )
-    elapsed_time = int((time.process_time() - start_time) * 1000)
+    elapsed_time = int((time.perf_counter() - start_time) * 1000.0)
     print("Serialization (ms): " + str(elapsed_time))
 
     return response
